6950a6b4bd22a25ddde7eee31814898f473fc3ec
BrunoAgst <bruno_agst@hotmail.com>
2020-04-20 21:36:09 -0300

    cadastrando as categorias no banco de dados

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category"); //carregando o model
+const slugify = require("slugify"); //carregando o slugfy
 
 //definimos todas as de categoria utilizando o recurso router e exportamos com o module.exports
 router.get("/admin/categories/new", (req, res) => {
@@ -7,5 +9,19 @@ router.get("/admin/categories/new", (req, res) => {
 });
 
 
+//definindo a rota para salvar no banco
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if(title != undefined){ //verificando o campo não está undefined
+       Category.create({
+           title: title,
+           slug: slugify(title) //versão do titulo otimizado para URL
+       }).then(() => {
+            res.redirect("/");
+       });
+    }else{
+        res.redirect("/admin/categories/new");
+    }
+});
 
 module.exports = router;
