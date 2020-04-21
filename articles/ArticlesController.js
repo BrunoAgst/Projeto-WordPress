0f08c5b4229adf95a6600b0e113d3bb3262baf55
BrunoAgst <bruno_agst@hotmail.com>
2020-04-21 02:15:24 -0300

    cadastrando o artigo no banco de dados

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
+
+
 
 //definimos todas as de categoria utilizando o recurso router e exportamos com o module.exports
-router.get("/articles", (req, res) => {
+router.get("/admin/articles", (req, res) => {
     res.send("ROTA DE ARTIGO")
 });
+
 
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(category => {
         res.render("admin/articles/new",{categories: category})    
     });
     
+});
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category //esse id é gerando quando criamos o relacionamento entre as tabelas, mais conhecido como chave estrangeira
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('wordpress', 'root', '15121996br.',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00' //definindo a timezone
 });
 
 module.exports = connection;
