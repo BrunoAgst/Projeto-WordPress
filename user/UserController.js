e6dae2b6c00b5a5902bb6542a04762ad3cb62e4b
BrunoAgst <bruno_agst@hotmail.com>
2020-04-22 22:58:45 -0300

    criando view de cadastro e view de listagem de usuários

--- user/UserController.js
+++ user/UserController.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/users", (req, res) => {
-    res.send("Listagem de usuários");
+    User.findAll().then(user => {
+        res.render("admin/users/index",{users: user});
+    });
 });
 
 router.get("/admin/users/create", (req, res) => {
-    res.render("admin/user/create");
+    res.render("admin/users/create");
+});
+
+
+router.post("/users/create", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOne({ //verificando se o e-mail já foi cadastrado no banco de dados
+        where: {
+            email: email
+        }
+    }).then(user => {
+        if(user == undefined){
+
+            var salt = bcrypt.genSaltSync(10); //algo a mais que voce coloca no hash para aumentar a segurança
+            var hash = bcrypt.hashSync(password, salt);//gerando o hsh
+            
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect("/");
+            }).catch((err) => {
+                res.redirect("/");
+            });
+
+        }else{
+            res.redirect("/admin/users/create")
+        }
+    })
 });
 
 module.exports = router
