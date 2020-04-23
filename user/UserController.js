e3e972048c71b602a145cdd45508ceb50e45de80
BrunoAgst <bruno_agst@hotmail.com>
2020-04-23 00:30:09 -0300

    autenticando o usuário

--- user/UserController.js
+++ user/UserController.js
@@ -43,4 +43,36 @@ router.post("/users/create", (req, res) => {
     })
 });
 
+router.get("/login", (req, res) => {
+    res.render("admin/users/login");
+});
+
+//rota de autenticação
+router.post("/authenticate", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOne({ //pesquisando se o e-mail existe no banco
+        where: {
+            email : email
+        }
+    }).then(user => {
+        if(user != undefined){ //verifica se existe um usuário com esse e-mail
+            //valida a senha
+            var correct = bcrypt.compareSync(password, user.password);
+            if(correct){ //verifica se está correta
+                req.session.user = { //cria a sessao de login
+                    id: user.id,
+                    email: user.email
+                }
+                res.json(req.session.user);
+            }else{
+                res.redirect("/login");
+            }
+        }else{
+            res.redirect("/login");
+        }
+    });
+});
+
 module.exports = router
